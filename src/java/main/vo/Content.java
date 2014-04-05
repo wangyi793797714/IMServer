5f664969863b6b61136782b215f6a9f62225ed1f
wangyi793797714 <1824440212@qq.com>
2014-04-05 22:24:46 +0800

    修复将本地未读消息和网络离线消息进行合并并且提示,在mapper中添加新增的字段

--- src/java/main/vo/Content.java
+++ src/java/main/vo/Content.java
@@ -7,134 +7,146 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class Content implements Serializable {
 
-    private int id;
-    
-    /** 发送者名字 */
-    private String sendName;
+	private int id;
 
-    /** 接受人姓名 */
-    private String receiveName;
+	/** 发送者名字 */
+	private String sendName;
 
-    /** 发送日期 */
-    private Date date;
+	/** 接受人姓名 */
+	private String receiveName;
 
-    /** 发送内容 */
-    private String msg;
+	/** 发送日期 */
+	private Date date;
 
-    /** true:发送出去；false：接受 */
-    private boolean isSendMsg;
+	/** 发送内容 */
+	private String msg;
 
-    /** 0：群发；!=0:点对点:目标channelId */
-    private int receiveId;
+	/** true:发送出去；false：接受 */
+	private boolean isSendMsg;
 
-    /** 发送消息的人员编号 */
-    private int sendId;
+	/** 0：群发；!=0:点对点:目标channelId */
+	private int receiveId;
 
-    /** 接收消息的对象编号 */
-    private List<Integer> targetIds;
+	/** 发送消息的人员编号 */
+	private int sendId;
 
-    /** 如果是群聊，那么指定当前的聊天内容是属于哪一聊天组的 */
-    private long grouppTag;
-    
-    /**用来标示当前消息的归属，主要用于进入聊天界面显示最近的10条信息*/
-    private String belongTo;
+	/** 接收消息的对象编号 */
+	private List<Integer> targetIds;
 
-    /**标示当前信息是否已经查阅*/
-    private String isRead;
-    
-    public Date getDate() {
-        return date;
-    }
+	/** 如果是群聊，那么指定当前的聊天内容是属于哪一聊天组的 */
+	private long grouppTag;
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	/** 用来标示当前消息的归属，主要用于进入聊天界面显示最近的10条信息 */
+	private String belongTo;
 
-    public String getMsg() {
-        return msg;
-    }
+	/** 标示当前信息是否已经查阅 */
+	private String isRead;
 
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
+	/** 标示当前消息是否在本地已经存在 */
+	private String isLocalMsg = "false";
 
-    public boolean isSendMsg() {
-        return isSendMsg;
-    }
+	public Date getDate() {
+		return date;
+	}
 
-    public void setSendMsg(boolean isSendMsg) {
-        this.isSendMsg = isSendMsg;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-    public String getSendName() {
-        return sendName;
-    }
+	public String getMsg() {
+		return msg;
+	}
 
-    public void setSendName(String sendName) {
-        this.sendName = sendName;
-    }
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
 
-    public String getReceiveName() {
-        return receiveName;
-    }
+	public boolean isSendMsg() {
+		return isSendMsg;
+	}
 
-    public void setReceiveName(String receiveName) {
-        this.receiveName = receiveName;
-    }
+	public void setSendMsg(boolean isSendMsg) {
+		this.isSendMsg = isSendMsg;
+	}
 
-    public int getReceiveId() {
-        return receiveId;
-    }
+	public String getSendName() {
+		return sendName;
+	}
 
-    public void setReceiveId(int receiveId) {
-        this.receiveId = receiveId;
-    }
+	public void setSendName(String sendName) {
+		this.sendName = sendName;
+	}
 
-    public int getSendId() {
-        return sendId;
-    }
+	public String getReceiveName() {
+		return receiveName;
+	}
 
-    public void setSendId(int sendId) {
-        this.sendId = sendId;
-    }
+	public void setReceiveName(String receiveName) {
+		this.receiveName = receiveName;
+	}
 
-    public List<Integer> getTargetIds() {
-        return targetIds;
-    }
+	public int getReceiveId() {
+		return receiveId;
+	}
 
-    public void setTargetIds(List<Integer> targetIds) {
-        this.targetIds = targetIds;
-    }
+	public void setReceiveId(int receiveId) {
+		this.receiveId = receiveId;
+	}
 
-    public long getGrouppTag() {
-        return grouppTag;
-    }
+	public int getSendId() {
+		return sendId;
+	}
 
-    public void setGrouppTag(long grouppTag) {
-        this.grouppTag = grouppTag;
-    }
+	public void setSendId(int sendId) {
+		this.sendId = sendId;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public List<Integer> getTargetIds() {
+		return targetIds;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setTargetIds(List<Integer> targetIds) {
+		this.targetIds = targetIds;
+	}
 
-    public String getBelongTo() {
-        return belongTo;
-    }
+	public long getGrouppTag() {
+		return grouppTag;
+	}
 
-    public void setBelongTo(String belongTo) {
-        this.belongTo = belongTo;
-    }
+	public void setGrouppTag(long grouppTag) {
+		this.grouppTag = grouppTag;
+	}
 
-    public String getIsRead() {
-        return isRead;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setIsRead(String isRead) {
-        this.isRead = isRead;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBelongTo() {
+		return belongTo;
+	}
+
+	public void setBelongTo(String belongTo) {
+		this.belongTo = belongTo;
+	}
+
+	public String getIsRead() {
+		return isRead;
+	}
+
+	public void setIsRead(String isRead) {
+		this.isRead = isRead;
+	}
+
+	public String getIsLocalMsg() {
+		return isLocalMsg;
+	}
+
+	public void setIsLocalMsg(String isLocalMsg) {
+		this.isLocalMsg = isLocalMsg;
+	}
+
 }
